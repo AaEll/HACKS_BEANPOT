03a5b7c1753309ccb11a0dc8a2bd1e2365b657bb
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2019-02-09 17:49:20 -0500

    added default royalty free wav file
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- musicalpdb.py
+++ musicalpdb.py
@@ -5,19 +5,6 @@ import wave
 import time
 
 
-
-data ={}
-length_of_partition = 90000
-wav_path = 'C:\\Users\\musicalpdb\\resources\\music\\LuckyYou.wav'#44mozarts15b.wav' #NWA.wav
-wave_read = wave.open(wav_path, 'rb')
-num_channels = wave_read.getnchannels()
-bytes_per_sample = wave_read.getsampwidth()
-sample_rate = wave_read.getframerate()
-no_frames = wave_read.getnframes()
-no_partitions = no_frames//length_of_partition
-for i in range(no_partitions):
-	data[i] = wave_read.readframes(length_of_partition)
-
 class infinite_array():
     def __init__(self,inp):
         self.standard_input = inp
@@ -31,11 +18,29 @@ class infinite_array():
 class myPdb(pdb.Pdb):
     play_obj = None
 
+    def inpWav(self, wav_path, length_of_partition):
+        """
+        :wav_path: TODO
+        :length_of_partition: TODO
+        :returns: TODO
+
+        """
+        self.data = {}
+        wave_read = wave.open(wav_path, 'rb')
+        num_channels = wave_read.getnchannels()
+        bytes_per_sample = wave_read.getsampwidth()
+        sample_rate = wave_read.getframerate()
+        no_frames = wave_read.getnframes()
+        no_partitions = no_frames//length_of_partition
+
+        for i in range(no_partitions):
+            self.data[i] = wave_read.readframes(length_of_partition)
+
     def precmd(self,line):
         line_no = self.curframe.f_lineno
         if self.play_obj:
             self.play_obj.wait_done()
-        self.play_obj = sa.play_buffer(data[line_no%no_partitions], num_channels, bytes_per_sample, sample_rate)
+        self.play_obj = sa.play_buffer(self.data[line_no%no_partitions], num_channels, bytes_per_sample, sample_rate)
         return line
 
     def postcmd(self, stop, line):
@@ -59,10 +64,11 @@ class myPdb(pdb.Pdb):
         self.cmdqueue  = infinite_array('s\r\n')
 
 
-
-def my_set_trace(*, header=None):
+def my_set_trace(*, wav_path='music/alex-skrindo-miza-thinkin.wav', length_of_partition=80000, header=None):
     mypdb = myPdb(skip = ['musicalpdb','importlib*'])
+
+    mypdb.inpWav(wav_path, length_of_partition)
+
     if header is not None:
         mypdb.message(header)
     mypdb.set_trace(sys._getframe().f_back)
-my_set_trace()
